fd223c606e40f3854d85156b713f153fa6dcd226
sandilem7789 <sandilem7789@gmail.com>
2021-01-19 18:58:20 +0200

    serving static files

--- hello.js
+++ hello.js
@@ -1,23 +1,36 @@
 var http = require("http");
+var fs = require("fs");
+
+function serveStaticFiles(res, path, contentType, responseCode) {
+    if (!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data) {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("500 - Internal Error");
+        } else {
+            res.writeHead(responseCode, { "Content-Type": contentType });
+            res.end(data);
+        }
+    });
+}
 
 http.createServer(function(req, res) {
     /* Normalize url by removing querystring, 
-     * optional trailing slash,
-     * and making it lowercase 
+     * optional trailing slash, and making it lowercase 
      */
     var path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
     switch (path) {
         case "":
-            res.writeHead(200, { "Content-Type": "text/plain" });
-            res.end("HomePage");
+            serveStaticFiles(res, "/public/home.html", "text/html");
             break;
         case "/about":
-            res.writeHead(200, { "Content-Type": "text/plain" });
-            res.end("About");
+            serveStaticFiles(res, "/public/about.html", "text/html");
+            break;
+        case "/img/man.png":
+            serveStaticFiles(res, "/img/man.png", "image/png");
             break;
         default:
-            res.writeHead(404, { "Content-Type": "text/plain" });
-            res.end("Oops, Not Found");
+            serveStaticFiles(res, "/public/404.html", "text/html", 404)
             break;
     }
 }).listen(8000);
